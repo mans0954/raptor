9a8c3bb527e73e8e87870e49dbca9a7424f696c5
philsmart <philsmart@28d5239d-996f-4a2a-8d03-63c60a9693f8>
2010-05-29 16:37:03 +0000

    small update

--- ICA/trunk/src/main/java/main/uk/ac/cf/dao/external/file/ShibbolethLFP.java
+++ ICA/trunk/src/main/java/main/uk/ac/cf/dao/external/file/ShibbolethLFP.java
@@ -2,8 +2,13 @@ package main.uk.ac.cf.dao.external.file;
 
 import main.uk.ac.cf.dao.external.AuthenticationInput;
 
-public class ShibbolethLFP implements AuthenticationInput{
-
+/**
+ * @author phil
+ *  
+ *  This is the primary Shibboleth log file parser.
+ *
+ */
+public class ShibbolethLFP implements AuthenticationInput{ 
 	public void parse() {
 		// TODO Auto-generated method stub
 		
