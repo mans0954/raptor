b280afd84fe94515ee429f43172b42280ef9fa21
Phil Smart <16583275+philsmart@users.noreply.github.com>
2010-12-13 18:54:07 +0000

    [maven-scm] copy for tag mua-0.2

---
+++ MUA/tags/mua-0.2/src/main/java/uk/ac/cardiff/raptormua/engine/statistics/ShibbolethMetadataNameFormatter.java
@@ -0,0 +1,179 @@
+/**
+ * Copyright (C) 2010 Cardiff University, Wales <smartp@cf.ac.uk>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/**
+ *
+ */
+package uk.ac.cardiff.raptormua.engine.statistics;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+import org.apache.log4j.Logger;
+import org.opensaml.DefaultBootstrap;
+import org.opensaml.saml2.metadata.OrganizationName;
+import org.opensaml.saml2.metadata.provider.FilesystemMetadataProvider;
+import org.opensaml.saml2.metadata.provider.MetadataProvider;
+import org.opensaml.saml2.metadata.provider.MetadataProviderException;
+import org.opensaml.xml.ConfigurationException;
+import org.opensaml.xml.parse.BasicParserPool;
+//import org.opensaml.saml2.metadata.provider.FileBackedHTTPMetadataProvider;
+//import org.opensaml.saml2.metadata.provider.MetadataProviderException;
+
+import uk.ac.cardiff.model.Entry;
+import uk.ac.cardiff.model.ShibbolethEntry;
+import uk.ac.cardiff.raptormua.exceptions.PreprocessorException;
+import uk.ac.cardiff.raptormua.runtimeutils.ReflectionHelper;
+
+
+
+/**
+ * @author philsmart
+ *
+ */
+public class ShibbolethMetadataNameFormatter implements StatisticsPreProcessor{
+
+	/** Parser manager used to parse XML. */
+    protected static BasicParserPool parser;
+
+    /* use SAML metadata off the filesystem*/
+    private FilesystemMetadataProvider provider;
+
+    /* class logger */
+	static Logger log = Logger.getLogger(ShibbolethMetadataNameFormatter.class);
+
+	private String mapFieldName;
+	private String mapToFieldName;
+	/** this is not a proper URI at the moment, just a UNC path*/
+	private String SAMLMetadataURI;
+
+	public ShibbolethMetadataNameFormatter(String SAMLMetadataURI){
+		this.SAMLMetadataURI = SAMLMetadataURI;
+		try {
+			loadSAMLMetadata();
+		} catch (MetadataProviderException e) {
+			log.error("ShibbolethMetadataNameFormatter could not load SAML metadata file "+e.getMessage());
+			e.printStackTrace();
+		} catch (ConfigurationException e) {
+			log.error("Error parsing SAML metadata file "+e.getMessage());
+			e.printStackTrace();
+		}
+	}
+
+
+	/* (non-Javadoc)
+	 * @see uk.ac.cardiff.raptormua.engine.statistics.StatisticsPreProcessing#preProcess(java.util.List)
+	 */
+	@Override
+	public Set<Entry> preProcess(Set<Entry> entries) throws PreprocessorException{
+		LinkedHashSet<Entry> processedList = new LinkedHashSet<Entry>();
+		log.debug("Entries into preprocessor: "+entries.size());
+		for (Entry entry : entries){
+			if (entry instanceof ShibbolethEntry){
+				ShibbolethEntry shibEntry = (ShibbolethEntry)entry;
+				Object result = ReflectionHelper.getValueFromObject(getMapFieldName(), shibEntry);
+				if (result instanceof String){
+					String resultAsString = (String) result;
+					String mapTo=resultAsString;
+					try {
+						mapTo = getOrganisationName(resultAsString);
+					} catch (Exception e) {
+						//if the mapping fails, keep the original, so leave as original non mapped resultAsString
+						log.error("Failed to map "+resultAsString);
+					}
+					ReflectionHelper.setValueOnObject(getMapToFieldName(), mapTo, shibEntry);
+					processedList.add(shibEntry);
+				}
+				else{
+					throw new PreprocessorException("Not a valid field for preprocessing, "+getMapFieldName());
+				}
+			}
+			else{
+				//just add it to the return list
+				processedList.add(entry);
+			}
+
+		}
+		log.debug("Entries out of preprocessor: "+entries.size());
+		return processedList;
+	}
+
+	/**
+	 * This method returns the organizational name of the entityID passed into it from the SAML metadata
+	 * This will fail if the organizational name is not the first name in the list of organizational names
+	 * can use https://idp.cardiff.ac.uk/shibboleth for testing.
+	 * @param entityID
+	 * @return
+	 * @throws MetadataProviderException
+	 */
+	private String getOrganisationName(String entityID) throws MetadataProviderException, NullPointerException{
+		//log.debug("Getting organisationName for "+entityID);
+		OrganizationName org = (OrganizationName)provider.getEntityDescriptor(entityID).getOrganization().getOrganizationNames().get(0);
+		//log.debug("Found organisationName '"+org.getName().getLocalString()+"'");
+		return org.getName().getLocalString();
+	}
+
+	private void loadSAMLMetadata() throws MetadataProviderException, ConfigurationException{
+		DefaultBootstrap.bootstrap();
+		parser = new BasicParserPool();
+		parser.setNamespaceAware(true);
+		//FileBackedHTTPMetadataProvider fileprovider = new FileBackedHTTPMetadataProvider("http://iam.cf.ac.uk/cufederation/metadata.cufederation.xml", 100,"/Users/philsmart/shibMetadata.xml");
+		provider = new FilesystemMetadataProvider(new File(SAMLMetadataURI));
+		provider.setParserPool(parser);
+		provider.initialize();
+		log.debug("Loaded SAML metatada "+provider+" into "+this.getClass());
+	}
+
+	/**
+	 *
+	 * @param changeFieldName
+	 */
+	public void setMapFieldName(String changeFieldName) {
+		this.mapFieldName = changeFieldName;
+	}
+
+	/**
+	 *
+	 * @return
+	 */
+	public String getMapFieldName() {
+		return mapFieldName;
+	}
+
+
+	public void setMapToFieldName(String mapToFieldName) {
+		this.mapToFieldName = mapToFieldName;
+	}
+
+
+	public String getMapToFieldName() {
+		return mapToFieldName;
+	}
+
+
+	public void setSAMLMetadataURI(String sAMLMetadataURI) {
+		SAMLMetadataURI = sAMLMetadataURI;
+	}
+
+
+	public String getSAMLMetadataURI() {
+		return SAMLMetadataURI;
+	}
+
+
+
+}
---
+++ MUA/tags/mua-0.2/src/main/java/uk/ac/cardiff/raptormua/service/impl/MUAProcessImpl.java
@@ -0,0 +1,83 @@
+/**
+ * Copyright (C) 2010 Cardiff University, Wales <smartp@cf.ac.uk>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/**
+ *
+ */
+package uk.ac.cardiff.raptormua.service.impl;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import uk.ac.cardiff.model.Graph.AggregatorGraphModel;
+import uk.ac.cardiff.model.wsmodel.Capabilities;
+import uk.ac.cardiff.model.wsmodel.StatisticalUnitInformation;
+import uk.ac.cardiff.raptormua.engine.MUAEngine;
+import uk.ac.cardiff.raptormua.service.MUAProcess;
+
+/**
+ * @author philsmart
+ *
+ */
+public class MUAProcessImpl implements MUAProcess{
+	static Logger log = LoggerFactory.getLogger(MUAProcessImpl.class);
+
+	private MUAEngine engine;
+
+	public void setEngine(MUAEngine engine) {
+		this.engine = engine;
+	}
+
+	public MUAEngine getEngine() {
+		return engine;
+	}
+
+	/* (non-Javadoc)
+	 * @see uk.ac.cardiff.raptormua.service.MUAProcess#poll()
+	 */
+	public void poll() {
+		engine.poll();
+
+	}
+
+	/* (non-Javadoc)
+	 * @see uk.ac.cardiff.raptormua.service.MUAProcess#performStatistics(java.lang.String)
+	 */
+	public AggregatorGraphModel performStatistic(String statisticName) {
+		log.info("WebSservice call for perform statistic {} ",statisticName);
+		return engine.performStatistic(statisticName);
+
+	}
+
+	/* (non-Javadoc)
+	 * @see uk.ac.cardiff.raptormua.service.MUAProcess#getCapabilities()
+	 */
+	public Capabilities getCapabilities() {
+		log.info("WebSservice call for get capabilities");
+		return engine.getCapabilities();
+	}
+
+	/* (non-Javadoc)
+	 * @see uk.ac.cardiff.raptormua.service.MUAProcess#setStatisticalUnit(uk.ac.cardiff.model.wsmodel.StatisticalUnitInformation)
+	 */
+	@Override
+	public void updateStatisticalUnit(StatisticalUnitInformation statisticalUnitInformation) {
+	    engine.updateStatisticalUnit(statisticalUnitInformation);
+
+	}
+
+
+}
