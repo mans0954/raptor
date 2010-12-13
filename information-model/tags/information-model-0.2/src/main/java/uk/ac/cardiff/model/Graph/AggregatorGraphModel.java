03de2f64508226f8a08b5b81b36bf5af45d7618e
Phil Smart <16583275+philsmart@users.noreply.github.com>
2010-12-13 18:31:20 +0000

    [maven-scm] copy for tag information-model-0.2

---
+++ information-model/tags/information-model-0.2/src/main/java/uk/ac/cardiff/model/Graph/AggregatorGraphModel.java
@@ -0,0 +1,75 @@
+package uk.ac.cardiff.model.Graph;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/*
+ * based on the Apache Trinidad ChartModel, for compatibility. However, does not extend the Apache Trinidad Model
+ * so as to remain view agnostic.
+ */
+
+public class AggregatorGraphModel {
+
+	private List<String> groupLabels;
+	private List<String> seriesLabels;
+	private List<List<Double>> chartValues;
+
+
+	public List<String> getGroupLabels() {
+		  return groupLabels;
+
+	}
+
+	public void addGroupLabel(String label){
+		if (getGroupLabels()==null)setGroupLabels(new ArrayList<String>());
+		getGroupLabels().add(label);
+	}
+
+	public void addSeriesLabel(String label){
+		if (getSeriesLabels()==null)setSeriesLabels(new ArrayList<String>());
+		getSeriesLabels().add(label);
+	}
+
+
+	public List<String> getSeriesLabels() {
+	    return seriesLabels;
+
+	}
+
+	public void addGroupValue(List<Double> values){
+		if (chartValues==null) setChartValues(new ArrayList<List<Double>>());
+		chartValues.add(values);
+	}
+
+
+	public List<List<Double>> getYValues() {
+
+		    return chartValues;
+
+
+	}
+
+	public void setYValues(List<List<Double>> values) {
+
+	}
+
+
+	public void setGroupLabels(List<String> groupLabels) {
+		this.groupLabels = groupLabels;
+	}
+
+	public void setSeriesLabels(List<String> seriesLabels) {
+		this.seriesLabels = seriesLabels;
+	}
+
+	public void setChartValues(List<List<Double>> chartValues) {
+		this.chartValues = chartValues;
+	}
+
+	public List<List<Double>> getChartValues() {
+		return chartValues;
+	}
+
+
+}
