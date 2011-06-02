790c9c4dd590f0fac79af2bfa1793b93ecd1c65a
Phil Smart <16583275+philsmart@users.noreply.github.com>
2011-06-02 16:16:42 +0000

    [maven-scm] copy for tag raptor-client-0.1.0

---
+++ raptor-client/tags/raptor-client-0.1.0/src/main/java/uk/ac/cardiff/raptor/attribute/filtering/AttrributeFilterEngine.java
@@ -0,0 +1,90 @@
+/**
+ * Copyright (C) 2010 Cardiff University, Wales <smartp@cf.ac.uk>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/**
+ *
+ */
+package uk.ac.cardiff.raptor.attribute.filtering;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+import uk.ac.cardiff.model.ServiceMetadata;
+import uk.ac.cardiff.model.event.Event;
+
+/**
+ * @author philsmart
+ *
+ */
+public class AttrributeFilterEngine {
+
+	/** class logger. */
+	private final Logger log = LoggerFactory.getLogger(AttrributeFilterEngine.class);
+
+	/**
+	 * Only runs against the deny rules, and only denies basic:ANY attributes,
+	 * does not deny individual values.
+	 *
+	 * @param attributeFilterPolicy
+	 * @param entries
+	 * @return
+	 */
+	public List<Event> filter(AttributeFilterPolicy attributeFilterPolicy, ServiceMetadata metadata, List<Event> events) {
+		log.debug("Applying attribute filter policy {} to event set", attributeFilterPolicy.getPolicyName());
+		List<Event> filteredEntries = clone(events);
+		for (Event event : filteredEntries) {
+			for (AttributeRule attributeRule : attributeFilterPolicy.getAttributeRules()) {
+				filterAttributes(event, attributeRule, metadata);
+
+			}
+		}
+		return filteredEntries;
+	}
+
+	/**
+	 * Only process deny rules on the level of basic:ANY attributes. Hence will
+	 * not deny individual values
+	 *
+	 * @param entry
+	 * @param attributeRule
+	 * @param metadata
+	 */
+	private void filterAttributes(Event event, AttributeRule attributeRule, ServiceMetadata metadata) {
+	    try{
+		if (attributeRule.shouldApply(event.getClass())){
+		    attributeRule.filterAttribute(event, metadata);
+		}
+	    }
+	    catch(AttributeFilterException e){
+	        log.error("Could not filter attribute",e);
+	    }
+	}
+
+
+	private  List<Event> clone(List<Event> events) {
+		List<Event> clonedSet = new ArrayList<Event>();
+		for (Event event : events) {
+			clonedSet.add(event.copy());
+		}
+		log.trace("The event set of {} events has been cloned into the event set of {} events",events.size(),clonedSet.size());
+		return clonedSet;
+	}
+
+
+}
---
+++ raptor-client/tags/raptor-client-0.1.0/src/main/java/uk/ac/cardiff/raptor/event/expansion/ResourceCategoryAttributeAssociationDefinition.java
@@ -0,0 +1,134 @@
+/**
+ * Copyright (C) 2010 Cardiff University, Wales <smartp@cf.ac.uk>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/**
+ *
+ */
+package uk.ac.cardiff.raptor.event.expansion;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.dao.DataAccessException;
+
+import uk.ac.cardiff.model.event.Event;
+import uk.ac.cardiff.model.resource.ResourceMetadata;
+import uk.ac.cardiff.raptor.store.dao.RaptorDataConnection;
+
+/**
+ * @author philsmart
+ *
+ */
+public class ResourceCategoryAttributeAssociationDefinition extends AttributeAssociationDefinition{
+
+
+    /** Class logger. */
+    private final Logger log = LoggerFactory.getLogger(ResourceCategoryAttributeAssociationDefinition.class);
+
+    /** The data connector used to acquire the attributes*/
+    private RaptorDataConnection dataConnection;
+
+
+    public void initialise(){
+    }
+
+
+
+    /**
+     * @param event
+     */
+    public boolean associate(Event event) {
+
+       ResourceMetadata resourceMetadata = null;
+       Object result =  dataConnection.runQueryUnique("from ResourceMetadata where resourceId=?", new Object[]{event.getResourceId()});
+       if (result !=null && result instanceof ResourceMetadata){
+           resourceMetadata = (ResourceMetadata) result;
+           if (resourceMetadata.isExternal()){
+               event.setResourceIdCategory(2);
+           }
+           else if (resourceMetadata.isInternal()){
+               event.setResourceIdCategory(1);
+           }
+           return true;
+       }
+       else{
+           ResourceMetadata resourceNew = new ResourceMetadata();
+           resourceNew.setExternal(true);
+           resourceNew.setInternal(false);
+           resourceNew.setResourceId(event.getResourceId());
+           event.setResourceIdCategory(2);
+           try{
+               dataConnection.save(resourceNew);
+           }
+           catch(DataAccessException e){
+               log.error("Could not save new resource metadata {}",e.getMessage());
+               return false;
+           }
+           return true;
+       }
+
+    }
+
+
+    /**
+     * @param classToAdd the classToAdd to set
+     */
+    public void setClassToAdd(Class<?> classToAdd) {
+        this.classToAdd = classToAdd;
+    }
+
+    /**
+     * @return the classToAdd
+     */
+    public Class<?> getClassToAdd() {
+        return classToAdd;
+    }
+
+    /**
+     * @param associateWithClass the associateWithClass to set
+     */
+    public void setAssociateWithClass(String associateWithClass) {
+        this.associateWithClass = associateWithClass;
+    }
+
+    /**
+     * @return the associateWithClass
+     */
+    public String getAssociateWithClass() {
+        return associateWithClass;
+    }
+
+
+
+    /**
+     * @param dataConnection the dataConnection to set
+     */
+    public void setDataConnection(RaptorDataConnection dataConnection) {
+        this.dataConnection = dataConnection;
+    }
+
+
+
+    /**
+     * @return the dataConnection
+     */
+    public RaptorDataConnection getDataConnection() {
+        return dataConnection;
+    }
+
+
+
+
+}
