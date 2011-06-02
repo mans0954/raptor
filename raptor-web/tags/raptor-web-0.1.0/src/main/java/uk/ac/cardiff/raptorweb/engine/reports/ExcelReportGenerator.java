79edf83d9ff43d29f54abbdc24278c3ba8bf1291
Phil Smart <16583275+philsmart@users.noreply.github.com>
2011-06-02 16:59:53 +0000

    [maven-scm] copy for tag raptor-web-0.1.0

---
+++ raptor-web/tags/raptor-web-0.1.0/src/main/java/uk/ac/cardiff/raptorweb/engine/reports/ExcelReportGenerator.java
@@ -0,0 +1,159 @@
+/**
+ * Copyright (C) 2010 Cardiff University, Wales <smartp@cf.ac.uk>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/**
+ *
+ */
+package uk.ac.cardiff.raptorweb.engine.reports;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
+import uk.ac.cardiff.raptorweb.engine.RaptorWebEngine;
+import uk.ac.cardiff.raptorweb.model.GraphModel;
+import uk.ac.cardiff.raptorweb.model.RaptorTableChartModel;
+import uk.ac.cardiff.raptorweb.model.ReportModel;
+import uk.ac.cardiff.raptorweb.model.TableSeries;
+import uk.ac.cardiff.raptorweb.model.WebSession;
+import uk.ac.cardiff.raptorweb.model.records.Row;
+import jxl.Workbook;
+import jxl.WorkbookSettings;
+import jxl.write.Label;
+import jxl.write.NumberFormats;
+import jxl.write.Number;
+import jxl.write.WritableCellFormat;
+import jxl.write.WritableFont;
+import jxl.write.WritableSheet;
+import jxl.write.WritableWorkbook;
+import jxl.write.WriteException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.io.Resource;
+
+/**
+ * @author philsmart
+ * 
+ */
+public class ExcelReportGenerator extends ReportConstructor {
+    static Logger log = LoggerFactory.getLogger(ExcelReportGenerator.class);
+
+    @Override
+    public void generateReport(WebSession session) {
+        log.info("Generating Excel Report {}", session.getGraphmodel().getSelectedStatisticalUnit());
+        String relativePath = null;
+        try {
+            // make sure base directory exists first
+            File baseGraphDirectory = saveDirectory.getFile();
+            if (!baseGraphDirectory.exists())
+                baseGraphDirectory.mkdir();
+
+            File dir = new File(saveDirectory.getFile().getCanonicalPath() + "/" + session.getUser().getName());
+            log.debug("Save Directory exists: " + dir.exists());
+            if (!dir.exists())
+                dir.mkdir();
+
+            DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss");
+            java.util.Date date = new java.util.Date();
+            
+            String fileName = session.getGraphmodel().getSelectedStatisticalUnit().getStatisticalUnitInformation().getStatisticParameters().getUnitName().replaceAll(" ", "") + "-"
+                    + dateFormat.format(date) + ".xls";
+            if (!session.getGraphmodel().getDownloadFilename().equals("")) {
+                fileName = session.getGraphmodel().getDownloadFilename()+".xls";
+            }
+            dir = new File(dir.getAbsoluteFile() + "/" + fileName);
+
+            WorkbookSettings ws = new WorkbookSettings();
+            ws.setLocale(new Locale("en", "EN"));
+            WritableWorkbook workbook = Workbook.createWorkbook(dir, ws);
+            WritableSheet s = workbook.createSheet("RaptorWeb Report " + session.getGraphmodel().getSelectedStatisticalUnit(), 0);
+
+            int rowCount = 0;
+            for (int lineCount = 0; lineCount < maxNoRows(session.getGraphmodel().getCurrentTableGraph()) + 1; lineCount++) {
+                int cellPosition = 0;
+                if (lineCount == 0) {
+                    // do headers
+                    for (TableSeries tseries : session.getGraphmodel().getCurrentTableGraph().getTableSeries()) {
+                        WritableFont wf = new WritableFont(WritableFont.ARIAL, 12, WritableFont.BOLD);
+                        WritableCellFormat cf = new WritableCellFormat(wf);
+                        cf.setWrap(false);
+                        Label h1 = new Label(cellPosition, 0, tseries.getSeriesLabel(), cf);
+                        s.addCell(h1);
+                        cellPosition++;
+                        Label h2 = new Label(cellPosition, 0, "Value", cf);
+                        s.addCell(h2);
+                        cellPosition++;
+                    }
+                } else {
+                    for (TableSeries tseries : session.getGraphmodel().getCurrentTableGraph().getTableSeries()) {
+                        if (tseries.getRows().size() > rowCount) {
+                            WritableFont wf = new WritableFont(WritableFont.ARIAL, 10, WritableFont.NO_BOLD);
+                            WritableCellFormat cf = new WritableCellFormat(wf);
+                            WritableCellFormat cf2 = new WritableCellFormat(NumberFormats.FLOAT);
+                            cf.setWrap(false);
+                            Label l = new Label(cellPosition, lineCount, tseries.getRows().get(rowCount).getGroup(), cf);
+                            s.addCell(l);
+                            cellPosition++;
+                            Number l2 = new Number(cellPosition, lineCount, ((Double) tseries.getRows().get(rowCount).getValue()), cf2);
+                            s.addCell(l2);
+                            cellPosition++;
+
+                        }
+                    }
+                    rowCount++;
+                }
+
+            }
+
+            relativePath = dir.getAbsolutePath().replace(baseDirectory.getFile().getParentFile().getAbsolutePath(), "");
+            Date now = new Date(System.currentTimeMillis());
+            session.getReportmodel().addReportForDownload(dir, relativePath, now, this.getHandledReportType().displayName);
+            log.debug("Excel Report Created At: " + relativePath);
+            workbook.write();
+            workbook.close();
+
+        } catch (IOException e) {
+            log.error("Problem generating excel report {}", e.getMessage());
+
+        } catch (WriteException e) {
+            log.error("Problem generating excel report {}", e.getMessage());
+        }
+
+        log.info("Excel Report Created" + session.getGraphmodel().getSelectedStatisticalUnit());
+    }
+
+    /**
+     * @param currentTableGraph
+     * @return
+     */
+    private int maxNoRows(RaptorTableChartModel currentTableGraph) {
+        int maxRows = 0;
+        for (TableSeries tseries : currentTableGraph.getTableSeries()) {
+            if (tseries.getRows().size() > maxRows)
+                maxRows = tseries.getRows().size();
+        }
+        return maxRows;
+    }
+
+    @Override
+    protected HandledReportTypes getRegisterHandledReportType() {
+        return HandledReportTypes.excel;
+    }
+}
---
+++ raptor-web/tags/raptor-web-0.1.0/src/main/java/uk/ac/cardiff/raptorweb/model/GraphModel.java
@@ -0,0 +1,244 @@
+/**
+ * Copyright (C) 2010 Cardiff University, Wales <smartp@cf.ac.uk>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package uk.ac.cardiff.raptorweb.model;
+
+import java.io.File;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+import uk.ac.cardiff.model.report.AggregatorGraphModel;
+import uk.ac.cardiff.model.report.Series;
+import uk.ac.cardiff.model.wsmodel.StatisticParameters;
+import uk.ac.cardiff.model.wsmodel.StatisticParameters.EventType;
+import uk.ac.cardiff.model.wsmodel.StatisticalUnitInformation;
+import uk.ac.cardiff.model.wsmodel.SuggestionValues;
+import uk.ac.cardiff.raptorweb.model.ChartOptions.ChartType;
+import uk.ac.cardiff.raptorweb.model.ChartOptions.GraphPresentation;
+
+
+
+public class GraphModel implements Serializable{
+
+    	/** Generated SerialVersionUID */
+    	private static final long serialVersionUID = -2803349385469406219L;
+
+	/** The raw graph model, for later reconstruction*/
+	private AggregatorGraphModel rawGraphModel;
+
+	private RaptorGraphModel currentGraph;
+	private ChartOptions chartOptions;
+	private RaptorTableChartModel currentTableGraph;
+	private StatisticalUnitInformationView selectedStatisticalUnit;
+	private String processingResult;
+	private boolean showControlPanel;
+	private RaptorJFreeChartModel currentJFreeGraph;
+	private String controlPanelSize;
+	private List<StatisticalUnitInformationView> statisticalUnitsForView;
+	
+	/** The filename to use for report downloads.*/
+	private String downloadFilename;
+
+	private SuggestionValues suggestionValues;
+
+	/** Selected series modal panel*/
+	private Series selectedSeries;
+
+
+
+	/**
+	 * Set some sensible defaults for this graphs models chart options
+	 */
+	public GraphModel(){
+	    chartOptions = new ChartOptions();
+	    chartOptions.setPerspective("false");
+	    chartOptions.setGraphType(ChartOptions.ChartType.BAR);
+	    chartOptions.setxMajorGridCount(-1);
+	    chartOptions.setyMajorGridCount(-1);
+	    chartOptions.setOrientation(ChartOptions.OrientationType.VERTICAL);
+	    chartOptions.setImageWidth(1480);
+	    chartOptions.setImageHeight(1024);
+	    chartOptions.setxLabelPosition(ChartOptions.LabelPositionType.UP_90);
+	    chartOptions.setGraphPresentation(GraphPresentation.FANCY);
+	    chartOptions.setGraphType(ChartType.BAR3D);
+
+	    //create a blank selected statistical unit for display
+	    selectedStatisticalUnit = new StatisticalUnitInformationView();
+
+	}
+
+	/**
+	 * Set the old statistical unit to false, update it with the newly selected statisticalunit
+	 * and set it to selected.
+	 *
+	 * @param selectedStatisticalUnit the selectedStatisticalUnit to set
+	 */
+	public void setSelectedStatisticalUnit(StatisticalUnitInformationView selectedStatisticalUnit) {
+	    	this.selectedStatisticalUnit.setSelected(false);
+	    	this.selectedStatisticalUnit = selectedStatisticalUnit;
+	    	this.selectedStatisticalUnit.setSelected(true);
+	}
+	
+	/** Find all fields for the currently selected event type 
+	 * 
+	 * @return
+	 */
+	public List<String> getPossibleFieldNameValues(){
+	    EventType eventType = selectedStatisticalUnit.getStatisticalUnitInformation().getStatisticParameters().getEventType();
+	    String[] classFilter = eventType.getClassHierarchy();
+	    return suggestionValues.getPossibleFieldNameValuesList(classFilter);	    	    
+	}
+
+
+
+	/**
+	 * @return the selectedStatisticalUnit
+	 */
+	public StatisticalUnitInformationView getSelectedStatisticalUnit() {
+		return selectedStatisticalUnit;
+	}
+
+	/**
+	 * @param invokeStatisticalUnit
+	 */
+	public void setCurrentGraph(RaptorGraphModel graph) {
+		this.currentGraph = graph;
+
+	}
+
+	/**
+	 *
+	 * @return the <code>RaptorGraphMode</code> associated with this graph view
+	 */
+	public RaptorGraphModel getCurrentGraph() {
+		return currentGraph;
+	}
+
+	public void setCurrentTableGraph(RaptorTableChartModel currentTableGraph) {
+		this.currentTableGraph = currentTableGraph;
+	}
+
+	public RaptorTableChartModel getCurrentTableGraph() {
+		return currentTableGraph;
+	}
+
+
+
+	public void setChartOptions(ChartOptions chartOptions) {
+	    this.chartOptions = chartOptions;
+	}
+
+
+
+	public ChartOptions getChartOptions() {
+	    return chartOptions;
+	}
+
+	public void setProcessingResult(String processingResult) {
+	    this.processingResult = processingResult;
+	}
+
+	public String getProcessingResult() {
+	    return processingResult;
+	}
+
+	public void setShowControlPanel(boolean showControlPanel) {
+	    this.showControlPanel = showControlPanel;
+	}
+
+	public boolean isShowControlPanel() {
+	    return showControlPanel;
+	}
+
+	public void toggleShowControlPanel(){
+	    showControlPanel = !showControlPanel;
+	    controlPanelSize="30%";
+	}
+
+	public void setCurrentJFreeGraph(RaptorJFreeChartModel currentJFreeGraph) {
+	    this.currentJFreeGraph = currentJFreeGraph;
+	}
+
+	public RaptorJFreeChartModel getCurrentJFreeGraph() {
+	    return currentJFreeGraph;
+	}
+
+	public void setControlPanelSize(String controlPanelSize) {
+	    this.controlPanelSize = controlPanelSize;
+	}
+
+	public String getControlPanelSize() {
+	    return controlPanelSize;
+	}
+
+	public void setStatisticalUnitsForView(List<StatisticalUnitInformationView> statisticalUnitsForView) {
+	    this.statisticalUnitsForView = statisticalUnitsForView;
+	}
+
+	public List<StatisticalUnitInformationView> getStatisticalUnitsForView() {
+	    return statisticalUnitsForView;
+	}
+
+	public void setSelectedSeries(Series selectedSeries) {
+	    this.selectedSeries = selectedSeries;
+	}
+
+	public Series getSelectedSeries() {
+	    return selectedSeries;
+	}
+
+	public void setSuggestionValues(SuggestionValues suggestionValues) {
+	    this.suggestionValues = suggestionValues;
+	}
+
+	public SuggestionValues getSuggestionValues() {
+	    return suggestionValues;
+	}
+
+	public void setRawGraphModel(AggregatorGraphModel rawGraphModel) {
+	    this.rawGraphModel = rawGraphModel;
+	}
+
+	public AggregatorGraphModel getRawGraphModel() {
+	    return rawGraphModel;
+	}
+
+    /**
+     * @param downloadFilename the downloadFilename to set
+     */
+    public void setDownloadFilename(String downloadFilename) {
+        this.downloadFilename = downloadFilename;
+    }
+
+    /**
+     * @return the downloadFilename
+     */
+    public String getDownloadFilename() {
+        return downloadFilename;
+    }
+
+
+
+
+
+
+
+
+}
