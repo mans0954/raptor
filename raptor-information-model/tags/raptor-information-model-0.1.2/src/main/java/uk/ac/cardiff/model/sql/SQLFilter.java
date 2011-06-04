6e22f2361d9d549e1b1bee6ee9f7d60881653659
Phil Smart <16583275+philsmart@users.noreply.github.com>
2011-06-04 18:13:44 +0000

    [maven-scm] copy for tag raptor-information-model-0.1.2

---
+++ raptor-information-model/tags/raptor-information-model-0.1.2/src/main/java/uk/ac/cardiff/model/sql/SQLFilter.java
@@ -0,0 +1,18 @@
+package uk.ac.cardiff.model.sql;
+
+import java.io.Serializable;
+
+/**
+ * Base class for an SQL filter. Follows very small subsets of the SQL-99 ISO
+ * (ISO/IEC 9075-2:1999 - Database Languages - SQL - Part 2: Foundation (SQL/Foundation))
+ * specification.
+ *
+ * @author philsmart
+ *
+ */
+public class SQLFilter implements Serializable{
+
+    /** Generated SerialUID*/
+    private static final long serialVersionUID = 9151153850715517472L;
+
+}
