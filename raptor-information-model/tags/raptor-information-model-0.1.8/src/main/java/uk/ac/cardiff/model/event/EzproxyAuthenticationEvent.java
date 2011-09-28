78b8be2bb8e5175ca6159301221c583a1c78a1c4
Phil Smart <16583275+philsmart@users.noreply.github.com>
2011-09-28 11:03:15 +0000

    [maven-scm] copy for tag raptor-information-model-0.1.8

---
+++ raptor-information-model/tags/raptor-information-model-0.1.8/src/main/java/uk/ac/cardiff/model/event/EzproxyAuthenticationEvent.java
@@ -0,0 +1,142 @@
+/**
+ *
+ */
+package uk.ac.cardiff.model.event;
+
+import java.lang.reflect.Method;
+
+import uk.ac.cardiff.utility.EqualsUtil;
+import uk.ac.cardiff.utility.HashCodeUtil;
+
+/**
+ * @author philsmart
+ *
+ */
+public class EzproxyAuthenticationEvent extends AuthenticationEvent{
+
+    private String requesterIp;
+    private String sessionId;
+
+    public EzproxyAuthenticationEvent(){
+        super();
+    }
+
+    protected EzproxyAuthenticationEvent(EzproxyAuthenticationEvent event){
+        super(event);
+        this.requesterIp = event.getRequesterIp();
+        this.sessionId = event.getSessionId();
+    }
+
+    /**
+     * Copy method. Alternative to clone.
+     */
+    public EzproxyAuthenticationEvent copy(){
+        return new EzproxyAuthenticationEvent(this);
+    }
+
+    public void setRequesterIp(String requesterIp) {
+	this.requesterIp = requesterIp;
+    }
+
+    public String getRequesterIp() {
+	return requesterIp;
+    }
+
+    /**
+     * @param sessionId the sessionId to set
+     */
+    public void setSessionId(String sessionId) {
+	this.sessionId = sessionId;
+    }
+
+    /**
+     * @return the sessionId
+     */
+    public String getSessionId() {
+	return sessionId;
+    }
+
+    public String toString() {
+	Method[] methods = this.getClass().getMethods();
+	StringBuilder builder = new StringBuilder();
+	builder.append(this.getClass() + "@[");
+	for (Method method : methods) {
+	    try {
+		if (method.getName().startsWith("get") && !method.getName().equals("getClass")) {
+		    this.getClass().getMethod(method.getName(), (Class[]) null);
+		    Object object = method.invoke(this, (Object[]) null);
+		    builder.append(method.getName() + " [" + object + "],");
+
+		}
+	    } catch (Exception e){
+		//do nothing
+	    }
+	}
+	builder.append("]");
+	return builder.toString();
+    }
+
+
+    /**
+     * create a unique hash, with as uniform a distribution as possible
+     */
+    public int hashCode(){
+        int hash = HashCodeUtil.SEED;
+
+        hash = HashCodeUtil.hash(hash,getEventTimeMillis());
+        hash = HashCodeUtil.hash(hash,getEventId());
+        hash = HashCodeUtil.hash(hash,getAuthenticationType());
+        hash = HashCodeUtil.hash(hash,getServiceHost());
+        hash = HashCodeUtil.hash(hash,getRequesterIp());
+        hash = HashCodeUtil.hash(hash,getSessionId());
+        hash = HashCodeUtil.hash(hash,getResourceHost());
+        hash = HashCodeUtil.hash(hash,getPrincipalName());
+        hash = HashCodeUtil.hash(hash,getEventType());
+        hash = HashCodeUtil.hash(hash,getServiceId());
+        hash = HashCodeUtil.hash(hash,getResourceId());
+
+
+        return hash;
+
+    }
+
+    /**
+     * For hibernate, so the hashcode can be persisted
+     * @return
+     */
+    public int getHashCode(){
+        return hashCode();
+    }
+
+    /**
+     * For hibernate, does nothing as the hascode is computed on the fly
+     * from the <code>hashCode</code> method
+     *
+     * @param hashCode
+     */
+    public void setHashCode(int hashCode){
+
+    }
+
+    public boolean equals(Object obj){
+        if ( this == obj ) return true;
+        if((obj == null) || (obj.getClass() != this.getClass()))
+            return false;
+        EzproxyAuthenticationEvent that = (EzproxyAuthenticationEvent)obj;
+        boolean areEqual =
+          EqualsUtil.areEqual(this.getEventTimeMillis(), that.getEventTimeMillis()) &&
+          EqualsUtil.areEqual(this.getEventId(), that.getEventId()) &&
+          EqualsUtil.areEqual(this.getAuthenticationType(), that.getAuthenticationType()) &&
+          EqualsUtil.areEqual(this.getServiceHost(), that.getServiceHost()) &&
+          EqualsUtil.areEqual(this.getRequesterIp(), that.getRequesterIp()) &&
+          EqualsUtil.areEqual(this.getSessionId(), that.getSessionId()) &&
+          EqualsUtil.areEqual(this.getResourceHost(), that.getResourceHost()) &&
+          EqualsUtil.areEqual(this.getServiceId(), that.getServiceId()) &&
+          EqualsUtil.areEqual(this.getEventType(), that.getEventType()) &&
+          EqualsUtil.areEqual(this.getResourceId(), that.getResourceId()) &&
+          EqualsUtil.areEqual(this.getPrincipalName(), that.getPrincipalName());
+
+        return areEqual;
+    }
+
+}
---
+++ raptor-information-model/tags/raptor-information-model-0.1.8/src/main/java/uk/ac/cardiff/model/wsmodel/SuggestionValues.java
@@ -0,0 +1,112 @@
+package uk.ac.cardiff.model.wsmodel;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SuggestionValues implements Serializable {
+
+    /**
+     * default serial UId
+     */
+    private static final long serialVersionUID = 7312337340123235532L;
+
+    /** Class logger */
+    private final Logger log = LoggerFactory.getLogger(SuggestionValues.class);
+
+    /** The names of all fields from the information model */
+    private List<Suggestion> possibleFieldNameValues;
+
+    /** The possible values of all known fields from the information model */
+    private List<Suggestion> possibleFieldValues;
+
+    private List<Suggestion> possiblePostProcessors;
+
+    public void setPossibleFieldNameValues(List<Suggestion> possibleFieldNameValues) {
+        this.possibleFieldNameValues = possibleFieldNameValues;
+    }
+
+    public List<Suggestion> getPossibleFieldNameValues() {
+        return possibleFieldNameValues;
+    }
+
+    public List<String> getPossibleFieldNameValuesList() {
+        ArrayList<String> fields = new ArrayList<String>();
+        if (possibleFieldNameValues == null)
+            return fields;
+        for (Suggestion entry : possibleFieldNameValues) {
+            fields.add(entry.getValue());
+        }
+        return fields;
+    }
+
+    public List<String> getPossibleFieldNameValuesList(String[] classFilters) {
+        ArrayList<String> fields = new ArrayList<String>();
+        if (possibleFieldNameValues == null)
+            return fields;
+        for (Suggestion entry : possibleFieldNameValues) {
+            for (String filter : classFilters) {
+                if (filter.equals(entry.getBase())) {
+                    fields.add(entry.getValue());
+                }
+            }
+        }
+        return fields;
+    }
+
+    public ArrayList<String> autocomplete(String fieldName) {
+        log.debug("Suggesting values for selectedfield {}", fieldName);
+        ArrayList<String> possibles = new ArrayList<String>();
+        for (Suggestion suggestion : possibleFieldValues) {
+            if (suggestion.getBase().equals(fieldName)) {
+                log.debug("Adding possible value {}", suggestion.getValue());
+                possibles.add(suggestion.getValue());
+            }
+        }
+        return possibles;
+    }
+
+    /**
+     * @param possibleFieldValues
+     *            the possibleFieldValues to set
+     */
+    public void setPossibleFieldValues(List<Suggestion> possibleFieldValues) {
+        this.possibleFieldValues = possibleFieldValues;
+    }
+
+    /**
+     * @return the possibleFieldValues
+     */
+    public List<Suggestion> getPossibleFieldValues() {
+        return possibleFieldValues;
+    }
+
+    /**
+     * @param possiblePostProcessors
+     *            the possiblePostProcessors to set
+     */
+    public void setPossiblePostProcessors(List<Suggestion> possiblePostProcessors) {
+        this.possiblePostProcessors = possiblePostProcessors;
+    }
+
+    /**
+     * @return the possiblePostProcessors
+     */
+    public List<Suggestion> getPossiblePostProcessors() {
+        return possiblePostProcessors;
+    }
+
+    public ArrayList<String> getPossiblePostProcessorValuesList() {
+        ArrayList<String> fields = new ArrayList<String>();
+        if (possiblePostProcessors == null)
+            return fields;
+        for (Suggestion entry : possiblePostProcessors) {
+            fields.add(entry.getValue());
+        }
+        return fields;
+    }
+
+}
