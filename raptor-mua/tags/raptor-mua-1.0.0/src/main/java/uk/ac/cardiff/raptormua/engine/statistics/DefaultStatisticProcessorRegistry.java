6de93a0ff1bf6029a093b8a5cf42aac834738432
Phil Smart <16583275+philsmart@users.noreply.github.com>
2011-12-08 13:10:03 +0000

    [maven-scm] copy for tag raptor-mua-1.0.0

---
+++ raptor-mua/tags/raptor-mua-1.0.0/src/main/java/uk/ac/cardiff/raptormua/engine/statistics/DefaultStatisticProcessorRegistry.java
@@ -0,0 +1,109 @@
+/**
+ * Copyright (C) 2010 Cardiff University, Wales <smartp@cf.ac.uk>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.ac.cardiff.raptormua.engine.statistics;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import uk.ac.cardiff.model.wsmodel.ProcessorInformation;
+import uk.ac.cardiff.raptormua.engine.statistics.processor.ProcessorTemplate;
+import uk.ac.cardiff.raptormua.engine.statistics.processor.ProcessorTemplate.SCOPE;
+
+public class DefaultStatisticProcessorRegistry implements StatisticProcessorRegistry {
+
+    /** class logger */
+    private final Logger log = LoggerFactory.getLogger(DefaultStatisticProcessorRegistry.class);
+
+    /** A list or register of all postprocessors */
+    private List<ProcessorTemplate> postprocessors;
+
+    /** Factory used to construct processors from templates */
+    private StatisticProcessorFactory processorFactory;
+
+    /**
+     * Calls the <code>processorFactory</code> to instantiate a processor based a matching template from the list
+     * <code>postprocessors</code> and the input <code>processorInformation</code>. Also sets the friendly name of the
+     * <code>processorInformation</code> for future reference. This is a synchronized method, only one active thread can
+     * call this method at a time.
+     * 
+     * @param processorInformation information about the processor to create.
+     * @throws StatisticPostProcessorFactoryException
+     */
+    public synchronized StatisticPostProcessor getProcessor(ProcessorInformation processorInformation)
+            throws ProcessorRegistryException, StatisticPostProcessorFactoryException {
+
+        for (ProcessorTemplate processorTemplate : postprocessors) {
+            if (processorTemplate.getProcessorClass().getCanonicalName()
+                    .equals(processorInformation.getProcessorClass())) {
+
+                // check singleton
+                if (processorTemplate.getScope().equals(SCOPE.SINGLETON)) {
+                    log.debug("Returning singleton bean reference [{}] for [{}]",
+                            processorTemplate.getSingletonBeanReference(), processorTemplate.getProcessorFriendlyName());
+                    StatisticPostProcessor processor = processorTemplate.getSingletonBeanReference();
+                    processor.setFriendlyName(processorTemplate.getProcessorFriendlyName());
+                    processorInformation.setFriendlyName(processorTemplate.getProcessorFriendlyName());
+                    return processor;
+                }
+                // check else, create new (almost prototype) instance
+                processorInformation.setFriendlyName(processorTemplate.getProcessorFriendlyName());
+                return processorFactory.getPostProcessor(processorTemplate, processorInformation.getMethodParameters());
+            }
+        }
+        throw new ProcessorRegistryException("No such processor with type " + processorInformation.getProcessorClass());
+
+    }
+
+    /**
+     * @param postprocessors the postprocessors to set
+     */
+    public void setPostprocessors(List<ProcessorTemplate> postprocessors) {
+        for (ProcessorTemplate processorTemplate : postprocessors) {
+            log.info("Registering postprocessor [{}] type [{}]", processorTemplate.getProcessorFriendlyName(),
+                    processorTemplate.getProcessorClass());
+        }
+        this.postprocessors = postprocessors;
+    }
+
+    public List<ProcessorTemplate> getStatisticProcessorTemplates() {
+        return postprocessors;
+    }
+
+    /**
+     * @return the postprocessors
+     */
+    public List<ProcessorTemplate> getPostprocessors() {
+        return postprocessors;
+    }
+
+    /**
+     * @param processorFactory the processorFactory to set
+     */
+    public void setProcessorFactory(StatisticProcessorFactory processorFactory) {
+        this.processorFactory = processorFactory;
+    }
+
+    /**
+     * @return the processorFactory
+     */
+    public StatisticProcessorFactory getProcessorFactory() {
+        return processorFactory;
+    }
+
+}
