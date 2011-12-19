ccc17c42d29aa0fdf4d3d4fb45d7beadfaca8b0d
Phil Smart <16583275+philsmart@users.noreply.github.com>
2011-12-19 17:18:02 +0000

    [maven-scm] copy for tag raptor-mua-1.0.1

---
+++ raptor-mua/tags/raptor-mua-1.0.1/src/main/java/uk/ac/cardiff/raptormua/engine/CapabilitiesConstructorTask.java
@@ -0,0 +1,206 @@
+/**
+ * Copyright (C) 2010 Cardiff University, Wales <smartp@cf.ac.uk>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.ac.cardiff.raptormua.engine;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.Callable;
+
+import org.joda.time.DateTime;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import uk.ac.cardiff.model.ServiceMetadata;
+import uk.ac.cardiff.model.resource.ResourceMetadata;
+import uk.ac.cardiff.model.wsmodel.Capabilities;
+import uk.ac.cardiff.model.wsmodel.EventTypeInformation;
+import uk.ac.cardiff.model.wsmodel.ProcessorInformation;
+import uk.ac.cardiff.model.wsmodel.StatisticalUnitInformation;
+import uk.ac.cardiff.model.wsmodel.SuggestionValues;
+import uk.ac.cardiff.raptor.runtimeutils.ReflectionHelper;
+import uk.ac.cardiff.raptor.store.EventStorageEngine;
+import uk.ac.cardiff.raptormua.engine.statistics.BaseStatistic;
+import uk.ac.cardiff.raptormua.engine.statistics.StatisticHandler;
+import uk.ac.cardiff.raptormua.engine.statistics.StatisticProcessorRegistry;
+import uk.ac.cardiff.raptormua.engine.statistics.processor.ProcessorTemplate;
+import uk.ac.cardiff.raptormua.runtimeutils.ResourceMetadataComparator;
+
+/**
+ * Task that constructs capabilities in a background worker thread, before returning control back to the callback class.
+ * 
+ */
+public final class CapabilitiesConstructorTask implements Callable<Boolean> {
+
+    /** Class logger. */
+    private final Logger log = LoggerFactory.getLogger(CapabilitiesConstructorTask.class);
+
+    /** What to call when the operations is complete **/
+    private final CallbackInterface<Capabilities> callback;
+
+    /** The statiscs handler used to extract statistical units to perform. */
+    private final StatisticHandler statisticsHandler;
+
+    /** The storage engine used to extract information about events stored. */
+    private final EventStorageEngine storageEngine;
+
+    /** The service metadata used to embedded in the capabilities. */
+    private final ServiceMetadata metadata;
+
+    private Capabilities capabilities;
+
+    /** Set containing the names of fields that should not be included in the list of possible field values */
+    private Set<String> excludeFieldNames;
+
+    public CapabilitiesConstructorTask(CallbackInterface<Capabilities> callbackInterface,
+            StatisticHandler statisticsHandlerToUse, EventStorageEngine storageEngineToUse,
+            ServiceMetadata metadataToUse) {
+        callback = callbackInterface;
+        statisticsHandler = statisticsHandlerToUse;
+        storageEngine = storageEngineToUse;
+        metadata = metadataToUse;
+    }
+
+    public Boolean call() throws Exception {
+        doConstructCapabilities();
+        callback.returnResult(capabilities);
+        return null;
+    }
+
+    private void doConstructCapabilities() {
+        long startTime = System.currentTimeMillis();
+        capabilities = new Capabilities();
+        capabilities.setMetadata(metadata);
+        addStatisticInformation();
+        addSuggestions();
+        addEventTotals();
+        addEventDateInformation();
+        addResourceInformation();
+        try {
+            String version =
+                    Version.getMajorVersion() + "." + Version.getMinorVersion() + "." + Version.getMicroVersion();
+            capabilities.setMuaVersion(version);
+        } catch (Throwable e) {
+            capabilities.setMuaVersion("Unknown");
+        }
+
+        long endTime = System.currentTimeMillis();
+        log.info("Constructed MUA Capabilities for [{}] in {}s", metadata.getEntityId(), ((endTime - startTime) / 1000));
+
+    }
+
+    public void addStatisticInformation() {
+        log.debug("Adding statistical information to capabilities");
+        ArrayList<StatisticalUnitInformation> stats = new ArrayList<StatisticalUnitInformation>();
+        List<BaseStatistic> su = statisticsHandler.getStatisticalUnits();
+        for (BaseStatistic entry : su) {
+            log.debug("Setting statistical unit information as: " + entry.getStatisticParameters().getUnitName());
+            StatisticalUnitInformation information = new StatisticalUnitInformation();
+            information.setStatisticParameters(entry.getStatisticParameters());
+            information.setPostprocessors(entry.getAttachProcessors());
+            stats.add(information);
+        }
+        capabilities.setStatisticalServices(stats);
+    }
+
+    private void addSuggestions() {
+        log.debug("Adding suggestion information to capabilities");
+        SuggestionValues suggestionValues = new SuggestionValues();
+        suggestionValues.setPossibleFieldNameValues(ReflectionHelper.getFieldsFromEntrySubClasses());
+        capabilities.setSuggestionValues(suggestionValues);
+
+        StatisticProcessorRegistry processorRegistry =
+                statisticsHandler.getStatisticRegistry().getStatisticProcessorRegistry();
+
+        List<ProcessorTemplate> templates = processorRegistry.getStatisticProcessorTemplates();
+        List<ProcessorInformation> possiblePostProcessors = new ArrayList<ProcessorInformation>();
+        for (ProcessorTemplate template : templates) {
+            ProcessorInformation information = new ProcessorInformation();
+            information.setFriendlyName(template.getProcessorFriendlyName());
+            information.setProcessorClass(template.getProcessorClass().getCanonicalName());
+            information.setMethodParameters(template.getMethodParameters());
+            possiblePostProcessors.add(information);
+
+        }
+
+        log.trace("Has set {} possible post processor suggestion values", possiblePostProcessors.size());
+        suggestionValues.setPossiblePostProcessors(possiblePostProcessors);
+
+        // set possible values for field names
+        List<String> possibleFieldNames = new ArrayList<String>();
+        if (excludeFieldNames == null) {
+            possibleFieldNames = suggestionValues.getPossibleFieldNameValuesList();
+        } else {
+            for (String fieldname : suggestionValues.getPossibleFieldNameValuesList()) {
+                if (!excludeFieldNames.contains(fieldname)) {
+                    possibleFieldNames.add(fieldname);
+                }
+            }
+        }
+        log.debug("Capabilities constructor setting possible field values (can cause issues)");
+        suggestionValues.setPossibleFieldValues(storageEngine.getPossibleValuesFor(possibleFieldNames));
+    }
+
+    private void addEventTotals() {
+        log.debug("Adding event totals to capabilities");
+        capabilities.setNumberOfAuthenticationsStored(storageEngine.getEventHandler().getNumberOfEvents());
+        List<String> eventTypes = ReflectionHelper.getAllEventClasses();
+        List<EventTypeInformation> eventsPerType = new ArrayList<EventTypeInformation>();
+        for (String eventType : eventTypes) {
+            String query = "SELECT count(*) from " + eventType;
+            try {
+                long count = (Long) storageEngine.getEventHandler().queryUnique(query, null);
+                log.debug("EventType {} has {} events", eventType, count);
+                eventsPerType.add(new EventTypeInformation(eventType, count));
+            } catch (Exception e) {
+                log.warn("EventType [{}] has no count (table probably does not exist yet, no problem)", eventType);
+            }
+        }
+        capabilities.setEventsPerType(eventsPerType);
+    }
+
+    private void addEventDateInformation() {
+        log.debug("Adding event dates to capabilities");
+        DateTime latest =
+                (DateTime) storageEngine.getEventHandler().queryUnique("SELECT max(eventTime) from Event", null);
+        DateTime earliest =
+                (DateTime) storageEngine.getEventHandler().queryUnique("SELECT min(eventTime) from Event", null);
+        if (latest != null && earliest != null) {
+            capabilities.setLatestEventTime(new Date(latest.getMillis()));
+            capabilities.setEarliestEventTime(new Date(earliest.getMillis()));
+        }
+    }
+
+    private void addResourceInformation() {
+        log.debug("Adding resource metadata to capabilities");
+        List<ResourceMetadata> resourceMetadata =
+                (List<ResourceMetadata>) storageEngine.getEventHandler().query("from ResourceMetadata", null);
+        log.debug("Setting {} resource metadata(s)", resourceMetadata.size());
+        Collections.sort(resourceMetadata, new ResourceMetadataComparator());
+        capabilities.setResourceMetadata(resourceMetadata);
+    }
+
+    /**
+     * @param excludeFieldNames the excludeFieldNames to set
+     */
+    public void setExcludeFieldNames(Set<String> excludeFieldNames) {
+        this.excludeFieldNames = excludeFieldNames;
+    }
+
+}
---
+++ raptor-mua/tags/raptor-mua-1.0.1/src/main/java/uk/ac/cardiff/raptormua/wsinterface/impl/MultiUnitAggregatorImpl.java
@@ -0,0 +1,166 @@
+/**
+ * Copyright (C) 2010 Cardiff University, Wales <smartp@cf.ac.uk>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/**
+ *
+ */
+
+package uk.ac.cardiff.raptormua.wsinterface.impl;
+
+import java.util.List;
+
+import javax.jws.WebService;
+
+import org.apache.cxf.binding.soap.SoapFault;
+
+import uk.ac.cardiff.model.AdministrativeFunction;
+import uk.ac.cardiff.model.report.AggregatorGraphModel;
+import uk.ac.cardiff.model.resource.ResourceMetadata;
+import uk.ac.cardiff.model.wsmodel.Capabilities;
+import uk.ac.cardiff.model.wsmodel.EventPushMessage;
+import uk.ac.cardiff.model.wsmodel.LogFileUpload;
+import uk.ac.cardiff.model.wsmodel.LogFileUploadResult;
+import uk.ac.cardiff.model.wsmodel.StatisticalUnitInformation;
+import uk.ac.cardiff.raptor.remoting.server.sei.MultiUnitAggregator;
+import uk.ac.cardiff.raptormua.engine.Version;
+import uk.ac.cardiff.raptormua.service.MUAProcess;
+
+/**
+ * The Class MultiUnitAggregatorImpl.
+ * 
+ * @author philsmart
+ */
+
+@WebService(endpointInterface = "uk.ac.cardiff.raptor.remoting.server.sei.MultiUnitAggregator")
+public class MultiUnitAggregatorImpl implements MultiUnitAggregator {
+
+    /** The mua process service. */
+    private MUAProcess processService;
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see uk.ac.cardiff.raptor.remoting.server.sei.MultiUnitAggregator#getVersion()
+     */
+    public String getVersion() {
+        try {
+            return Version.getMajorVersion() + "." + Version.getMinorVersion() + "." + Version.getMicroVersion();
+        } catch (Throwable e) {
+            return "Unknown";
+        }
+    }
+
+    /**
+     * Sets the process service.
+     * 
+     * @param processService the new process service
+     */
+    public void setProcessService(MUAProcess processService) {
+        this.processService = processService;
+    }
+
+    /**
+     * Gets the process service.
+     * 
+     * @return the process service
+     */
+    public MUAProcess getProcessService() {
+        return processService;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see uk.ac.cardiff.raptor.remoting.server.sei.MultiUnitAggregator#getCapabilities()
+     */
+    public Capabilities getCapabilities() {
+        return processService.getCapabilities();
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see uk.ac.cardiff.raptor.remoting.server.sei.MultiUnitAggregator#invokeStatisticalUnit(java.lang.String)
+     */
+    public AggregatorGraphModel invokeStatisticalUnit(String statisticName) throws SoapFault {
+        return processService.performStatistic(statisticName);
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * uk.ac.cardiff.raptor.remoting.server.sei.MultiUnitAggregator#updateStatisticalUnit(uk.ac.cardiff.model.wsmodel
+     * .StatisticalUnitInformation)
+     */
+    public void updateStatisticalUnit(StatisticalUnitInformation statisticalUnitInformation) throws SoapFault {
+        processService.updateStatisticalUnit(statisticalUnitInformation);
+
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * uk.ac.cardiff.raptor.remoting.server.sei.MultiUnitAggregator#performAdministrativeFunction(uk.ac.cardiff.model
+     * .AdministrativeFunction)
+     */
+    public boolean performAdministrativeFunction(AdministrativeFunction function) throws SoapFault {
+        return processService.performAdministrativeFunction(function);
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see uk.ac.cardiff.raptor.remoting.server.sei.MultiUnitAggregator#addAuthentications(uk.ac.cardiff.model.wsmodel.
+     * EventPushMessage)
+     */
+    public void addAuthentications(EventPushMessage pushed) throws SoapFault {
+        processService.addAuthentications(pushed);
+
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * uk.ac.cardiff.raptor.remoting.server.sei.MultiUnitAggregator#updateAndInvokeStatisticalUnit(uk.ac.cardiff.model
+     * .wsmodel.StatisticalUnitInformation)
+     */
+    public AggregatorGraphModel updateAndInvokeStatisticalUnit(StatisticalUnitInformation statisticalUnitInformation)
+            throws SoapFault {
+        return processService.updateAndInvokeStatisticalUnit(statisticalUnitInformation);
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see uk.ac.cardiff.raptor.remoting.server.sei.MultiUnitAggregator#batchUpload(java.util.List)
+     */
+    public List<LogFileUploadResult> batchUpload(List<LogFileUpload> uploadFiles) throws SoapFault {
+        return processService.batchUpload(uploadFiles);
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see uk.ac.cardiff.raptor.remoting.server.sei.MultiUnitAggregator#saveResourceMetadata(java.util.List)
+     */
+    public void saveResourceMetadata(List<ResourceMetadata> resourceMetadata) {
+        processService.saveResourceMetadata(resourceMetadata);
+
+    }
+
+}
