212aad0ca8e8b9d00a7168dac92a7e8f52f128eb
philsmart <philsmart@28d5239d-996f-4a2a-8d03-63c60a9693f8>
2012-07-26 15:38:50 +0000

    [maven-scm] copy for tag raptor-client-1.1.0

---
+++ raptor-client/tags/raptor-client-1.1.0/src/main/java/uk/ac/cardiff/raptor/remoting/client/DefaultEventReleaseClient.java
@@ -0,0 +1,83 @@
+/**
+ * Copyright (C) 2010 Cardiff University, Wales <smartp@cf.ac.uk>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.ac.cardiff.raptor.remoting.client;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import uk.ac.cardiff.model.ServiceMetadata;
+import uk.ac.cardiff.model.event.Event;
+
+/**
+ * The Class DefaultEventReleaseClient.
+ */
+public class DefaultEventReleaseClient extends AbstractEventReleaseClient {
+
+    /** Class logger. */
+    private final Logger log = LoggerFactory.getLogger(DefaultEventReleaseClient.class);
+
+    /** The engine that releases events to a client endpoint. */
+    private EventReleaseEngine eventReleaseEngine;
+
+    /**
+     * Instantiates a new default event release client.
+     */
+    public DefaultEventReleaseClient() {
+        eventReleaseEngine = new EventReleaseEngine();
+        enableEventRelease = true;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see uk.ac.cardiff.raptor.remoting.client.EventReleaseClient#release(java.util.List,
+     * uk.ac.cardiff.model.ServiceMetadata)
+     */
+    public boolean release(final List<Event> events, final ServiceMetadata serviceMetadata)
+            throws ReleaseFailureException {
+        boolean success = false;
+        if (enableEventRelease) {
+            log.info("[Event Release Called]");
+            success = eventReleaseEngine.release(endpointRegistry, events, serviceMetadata);
+            endpointRegistry.storeReleaseInformationIfEnabled();
+            log.info("[--Events released to all listening endpoints {}--]", success);
+        }
+
+        return success;
+    }
+
+    /**
+     * Sets the event release engine.
+     * 
+     * @param eventReleaseEngine the new event release engine
+     */
+    public void setEventReleaseEngine(EventReleaseEngine eventReleaseEngine) {
+        this.eventReleaseEngine = eventReleaseEngine;
+    }
+
+    /**
+     * Gets the event release engine.
+     * 
+     * @return the event release engine
+     */
+    public EventReleaseEngine getEventReleaseEngine() {
+        return eventReleaseEngine;
+    }
+
+}
---
+++ raptor-client/tags/raptor-client-1.1.0/src/test/java/uk/ac/cardiff/raptor/event/expansion/RDBMSDataConnectorTest.java
@@ -0,0 +1,70 @@
+/**
+ * Copyright (C) 2010 Cardiff University, Wales <smartp@cf.ac.uk>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.ac.cardiff.raptor.event.expansion;
+
+import java.util.Map;
+
+import org.apache.commons.dbcp.BasicDataSource;
+import org.springframework.util.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import uk.ac.cardiff.raptor.event.expansion.connector.AttributeAssociationException;
+import uk.ac.cardiff.raptor.event.expansion.connector.RDBMSDataConnector;
+
+public class RDBMSDataConnectorTest {
+
+    private RDBMSDataConnector dataConnector;
+
+    //@BeforeClass
+    public void setup() {
+        BasicDataSource bds = new BasicDataSource();
+        bds.setDriverClassName("org.postgresql.Driver");
+        bds.setUrl("jdbc:postgresql://localhost/identities");
+        bds.setUsername("postgres");
+        bds.setPassword("");
+
+        dataConnector = new RDBMSDataConnector();
+        dataConnector.setDataSource(bds);
+        dataConnector.setNoResultIsError(false);
+        dataConnector.setCacheResults(true);
+        dataConnector.setCacheTimeoutMs(1000);
+        dataConnector.initialise();
+    }
+
+   // @Test
+    public void getAttributesTest() throws AttributeAssociationException {
+        dataConnector.setSearchTemplate("SELECT * FROM mock_identities_table where cn='[principal]'");
+        Map<String, String> attributes = dataConnector.lookup("scmps2");
+        for (Map.Entry<String, String> attribute : attributes.entrySet()) {
+            Assert.notNull(attribute);
+            System.out.println(attribute.getKey() + "," + attribute.getValue());
+        }
+    }
+
+   // @Test
+    public void cacheTest() throws AttributeAssociationException {
+        dataConnector.setSearchTemplate("SELECT * FROM mock_identities_table where cn='[principal]'");
+        Map<String, String> attributes = dataConnector.lookup("scmps2");
+        Map<String, String> attributesCached = dataConnector.lookup("scmps2");
+        for (Map.Entry<String, String> attribute : attributes.entrySet()) {
+            Assert.notNull(attribute);
+            System.out.println(attribute.getKey() + "," + attribute.getValue());
+        }
+    }
+
+}
