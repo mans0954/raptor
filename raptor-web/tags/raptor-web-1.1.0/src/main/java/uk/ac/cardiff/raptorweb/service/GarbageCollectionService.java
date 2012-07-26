a55c90655493f5249f39857e98c8c7e11918d024
philsmart <philsmart@28d5239d-996f-4a2a-8d03-63c60a9693f8>
2012-07-26 17:17:28 +0000

    [maven-scm] copy for tag raptor-web-1.1.0

---
+++ raptor-web/tags/raptor-web-1.1.0/src/main/java/uk/ac/cardiff/raptorweb/service/GarbageCollectionService.java
@@ -0,0 +1,33 @@
+/**
+ * Copyright (C) 2010 Cardiff University, Wales <smartp@cf.ac.uk>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/**
+ * 
+ */
+package uk.ac.cardiff.raptorweb.service;
+
+/**
+ * This service handles all functions for performing various garbage collection tasks in raptor-web (usually as background threads trigger by a scheduler).
+ * 
+ */
+public interface GarbageCollectionService {
+
+    /**
+     * Each new graph image in raptor is saved with a unique filename so the browser does not cache the image. These images are cleaned (removed) based on the
+     * criteria specified by the implementing method.
+     */
+    public void cleanOldGraphsDirectory();
+
+}
