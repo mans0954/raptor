4a2eeee6df35009aa676668714ee5df360d50e2c
philsmart <philsmart@28d5239d-996f-4a2a-8d03-63c60a9693f8>
2013-06-06 10:04:42 +0000

    [maven-scm] copy for tag raptor-client-1.1.5

---
+++ raptor-client/tags/raptor-client-1.1.5/src/main/java/uk/ac/cardiff/raptor/remoting/server/sei/MultiUnitAggregator.java
@@ -0,0 +1,145 @@
+/**
+ * Copyright (C) 2010 Cardiff University, Wales <smartp@cf.ac.uk>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.ac.cardiff.raptor.remoting.server.sei;
+
+import java.util.List;
+
+import javax.jws.WebService;
+
+import org.apache.cxf.binding.soap.SoapFault;
+
+import uk.ac.cardiff.model.AdministrativeFunction;
+import uk.ac.cardiff.model.report.AggregatorGraphModel;
+import uk.ac.cardiff.model.resource.ResourceMetadata;
+import uk.ac.cardiff.model.wsmodel.Capabilities;
+import uk.ac.cardiff.model.wsmodel.DynamicStatisticalUnitInformation;
+import uk.ac.cardiff.model.wsmodel.EventPushMessage;
+import uk.ac.cardiff.model.wsmodel.LogFileUpload;
+import uk.ac.cardiff.model.wsmodel.LogFileUploadResult;
+import uk.ac.cardiff.model.wsmodel.StatisticFunctionType;
+import uk.ac.cardiff.model.wsmodel.StatisticalUnitInformation;
+
+/**
+ * @author philsmart
+ * 
+ */
+
+@WebService
+public interface MultiUnitAggregator {
+
+    /**
+     * Gets the internal version number of this MUA. This does not have to correspond to the Maven or SCM raptor-mua
+     * release version
+     * 
+     * @return the internal version number of this MUA
+     */
+    public String getVersion();
+
+    /**
+     * Returns the capabilities of this MUA. This includes:
+     * <ol>
+     * <li>A list of statistical operations this MUA can perform.</li>
+     * <li>The Service Metadata associated to this MUA.</li>
+     * <li>The number of events this MUA has stored</li>
+     * <li>Metadata describing the all resource this MUA has 'seen'.</li>
+     * <li>Flags and error messages describing processing status</li>
+     * </ol>
+     * 
+     * @return
+     */
+    public Capabilities getCapabilities();
+
+    /**
+     * Updates the statistical unit described in the <code>statisticalUnitInformation</code> parameter.
+     * 
+     * @param statisticalUnitInformation information about the statistical unit to update
+     * @throws SoapFault
+     */
+    public void updateStatisticalUnit(StatisticalUnitInformation statisticalUnitInformation) throws SoapFault;
+
+    /**
+     * Invokes the statistical unit with name <code>statisticName</code> and returns the result of that unit as a
+     * <code>AggregatorGraphModel</code>
+     * 
+     * @param statisticName the <code>String</code> name of the statistical unit to invoke
+     * @return the result of invoking the statistical unit as a <code>AggregatorGraphModel</code>
+     * @throws SoapFault
+     */
+    public AggregatorGraphModel invokeStatisticalUnit(String statisticName) throws SoapFault;
+
+    /**
+     * A single shot interface that both updates and then performs a statistic (returning the result) in one go. This is
+     * useful if you want to perform both operations while there is a lock on the class so as other operations do not
+     * take place between update and invoke.
+     * 
+     * @param statisticalUnitInformation
+     * @return
+     */
+    public AggregatorGraphModel updateAndInvokeStatisticalUnit(StatisticalUnitInformation statisticalUnitInformation)
+            throws SoapFault;
+
+    /**
+     * Asks this MUA to perform an administrative function e.g. removeall stored events.
+     * 
+     * @param function information about the function to peform
+     * @return true if the administrative function performed successfully, false otherwise
+     * @throws SoapFault
+     */
+    public boolean performAdministrativeFunction(AdministrativeFunction function) throws SoapFault;
+
+    /**
+     * Adds events to this MUA
+     * 
+     * @param pushed the <code>EventPushMessage</code> that the events this MUA is to store are encapsulated in
+     * @throws SoapFault
+     */
+    public void addAuthentications(EventPushMessage pushed) throws SoapFault;
+
+    /**
+     * Send a batch of log files to the MUA to store
+     * 
+     * @param uploadFiles the files to parse and store events from
+     * @return
+     */
+    public List<LogFileUploadResult> batchUpload(List<LogFileUpload> uploadFiles);
+
+    /**
+     * Save resource information in the <code>resourceMetadata</code> parameter to this MUA's persistent store.
+     * 
+     * @param resourceMetadata
+     */
+    public void saveResourceMetadata(List<ResourceMetadata> resourceMetadata);
+
+    /**
+     * Allows the dynamic (runtime) instantiation, configuration and invocation of a statistical function.
+     * 
+     * @param dynamicStatisticalInformation
+     * @return
+     */
+    public AggregatorGraphModel invokeStatisticalUnitDynamically(
+            DynamicStatisticalUnitInformation dynamicStatisticalInformation);
+
+    /**
+     * Returns the statistical information for the <code>statisticType</code> input. Does this by dynamically
+     * constructing the BaseStatistic and sending its parameters back.
+     * 
+     * @param statisticType
+     * @return
+     */
+    public StatisticalUnitInformation getStatisticalUnitInformation(StatisticFunctionType statisticType);
+
+}
---
+++ raptor-client/tags/raptor-client-1.1.5/src/main/java/uk/ac/cardiff/raptor/store/QueryableEventHandler.java
@@ -0,0 +1,83 @@
+/**
+ * Copyright (C) 2010 Cardiff University, Wales <smartp@cf.ac.uk>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.ac.cardiff.raptor.store;
+
+import java.util.List;
+
+import uk.ac.cardiff.raptor.store.dao.StorageException;
+
+public interface QueryableEventHandler extends EventHandler {
+
+    /**
+     * Performs the update query <code>query</code> with the parameters <code>parameters</code>. Concrete
+     * implementations of the interface may require the query in a specific, specified, language e.g. HQL or JPQL.
+     * 
+     * @param query the query to run as an update.
+     * @param parameters the parameters to substitute into the query string <code>query</code>
+     * @return a <code>List</code> of objects that are the result of running the query <code>query</code> with the
+     *         parameters <code>parameters</code>
+     */
+    public void update(String query, Object[] parameters) throws StorageException;
+
+    /**
+     * Runs the query <code>query</code> with the parameters <code>parameters</code> and returns a <code>List</code> of
+     * results. Concrete implementations of the interface may require the query in a specific, specified, language e.g.
+     * HQL or JPQL.
+     * 
+     * @param query the query to run.
+     * @param parameters the parameters to substitute into the query string <code>query</code>
+     * @return a <code>List</code> of objects that are the result of running the query <code>query</code> with the
+     *         parameters <code>parameters</code>
+     */
+    public List query(String query, Object[] parameters);
+
+    /**
+     * Runs the query <code>query</code> with the parameters <code>parameters</code> and returns a <code>List</code> of
+     * results up to the specified <code>maxNoResults</code>. Concrete implementations of the interface may require the
+     * query in a specific, specified, language e.g. HQL or JPQL.
+     * 
+     * @param query the query to run.
+     * @param parameters the parameters to substitute into the query string <code>query</code>
+     * @return a <code>List</code> of objects that are the result of running the query <code>query</code> with the
+     *         parameters <code>parameters</code>
+     */
+    public List query(String query, Object[] parameters, int maxNoResults);
+
+    /**
+     * Runs the query <code>query</code> with the parameters <code>parameters</code> and returns a single unique result.
+     * Concrete implementations of the interface may require the query in a specific, specified, language e.g. HQL or
+     * JPQL.
+     * 
+     * @param query the query to run.
+     * @param parameters the parameters to substitute into the query string <code>query</code>
+     * @return a single object that is the result of running the query <code>query</code> with the parameters
+     *         <code>parameters</code>
+     */
+    public Object queryUnique(String query, Object[] parameters);
+
+    /**
+     * Runs the query <code>query</code> with the parameters <code>parameters</code> and returns a single unique result.
+     * Only the first result is taken even if the query would return more than one result. Concrete implementations of
+     * the interface may require the query in a specific, specified, language e.g. HQL or JPQL.
+     * 
+     * @param query the query to run.
+     * @param parameters the parameters to substitute into the query string <code>query</code>
+     * @return
+     */
+    public Object queryForceUnique(String query, Object[] object);
+
+}
